12fea3dc1f455e7b0a379607e4fb69e53c73b1e2
Sunil <skumar@constellant.com>
2022-01-03 18:47:59 +0530

    router

--- angular-crud/src/app/app-routing.module.ts
+++ angular-crud/src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import signin component
+import { SigninComponent } from './pages/account/signin/signin.component';
+//import register component
+import { RegisterComponent } from './pages/account/register/register.component';
+//import home component
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
-
+//declare page path using const
+const routes: Routes =[
+  {
+    path:'', //use empty route for set default page property
+    component:SigninComponent
+  },
+  {
+    path:'signup',
+    component:RegisterComponent
+  },
+  {
+   path:'home',
+   component:HomeComponent
+  },
+  //when user enter wrong url it will redirect set as default page
+  {
+    path: '**',
+    redirectTo: ''
+  },
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- angular-crud/src/app/app.module.ts
+++ angular-crud/src/app/app.module.ts
@@ -5,40 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 //import reactive form
 import { ReactiveFormsModule } from '@angular/forms';
-//import routes
-import { RouterModule, Routes } from '@angular/router';
 //import signin component
 import { SigninComponent } from './pages/account/signin/signin.component';
 //import register component
 import { RegisterComponent } from './pages/account/register/register.component';
-
-//declare page path using const
-const appRoutes:Routes=[
-  {
-    path:'', //use empty route for set default page property
-    component:SigninComponent
-  },
-  {
-    path:'signup',
-    component:RegisterComponent
-  }
-]
-
+//import home component
+import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     //declare signin component
     SigninComponent,
     //declare register component
-    RegisterComponent
+    RegisterComponent,
+    //declare register component
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     //imports reactive form module
-    ReactiveFormsModule,
-    //import router module
-    RouterModule.forRoot(appRoutes)
+    ReactiveFormsModule,   
   ],
   providers: [],
   bootstrap: [AppComponent]
